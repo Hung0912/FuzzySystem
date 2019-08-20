0177af2c8919b1c2bf7ff0107e37c444b4dc5fc4
Hung Phan Bao <hunghusthedspi@gmail.com>
2019-08-20 16:30:17 +0700

    fuzzy relation

--- fuzzylogic.py
+++ fuzzylogic.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sys
+
 #membership function
 
 def triangular(x, a, b, c):
@@ -16,6 +18,12 @@ def triangular2(array, a, b, c):
         result_array.append(max(min((x-a)/(b-a), (c-x)/(c-b)), 0))
     return result_array
 
+def complex_fuzz(r, w):
+    if 0. <= r <= 1. :
+        return r * np.exp(1j * w)
+    else:
+        return 0
+
 def trapezoid2(array, a, b, c, d):
     result_array = []
     for x in array:
@@ -27,3 +35,14 @@ def gaussian2(array, mean, sigma):
     for x in array:
         result_array.append(np.exp(-np.power(x - mean, 2.) / (2 * np.power(sigma, 2.))))  
     return result_array
+
+def bell(x, a, b, c):
+    return 1/(1+np.power(np.abs((x-c)/a),2*b))
+
+def dil(x):
+    return np.power(x, 0.5)
+
+def con(x):
+    return np.power(x, 2.0)
+
+print(complex_fuzz(0.4, 0.1 * np.pi))
--- test.py
+++ test.py
@@ -12,48 +12,80 @@ short = fuzz.gaussian2(height, 1.40, 2.0)
 average = fuzz.gaussian2(height, 1.65, 2.0)
 tall = fuzz.gaussian2(height, 1.8, 2.0)
 #Weight
-skin = fuzz.triangular2(weight, 30, 50, 65)
-fat = fuzz.triangular2(weight, 50, 70, 90)
+light = fuzz.triangular2(weight, 30, 50, 65)
+heavy = fuzz.triangular2(weight, 50, 70, 90)
 
 #Output Universe
-size = np.arange(0, 10, 0.1)
+BMI = np.arange(0, 50, 0.1)
 #Output membership function
-sizeS = fuzz.triangular2(size, 0, 2, 5)
-sizeM = fuzz.triangular2(size, 4, 6, 8)
-sizeL = fuzz.triangular2(size, 6, 8, 10)
+under_weight = fuzz.triangular2(BMI, 0., 8., 20.)
+normal = fuzz.triangular2(BMI, 18.0, 21.0, 27.0)
+over_weight = fuzz.triangular2(BMI, 25.0, 27.0, 33.0)
+obese = fuzz.triangular2(BMI, 30.0, 32.0, 38.0)
+extremly_obese = fuzz.triangular2(BMI, 35.0, 40.0, 50.0)
 
-#Apply inputs
+#Fuzzy inputs
 def height_category(height_in):
-    height_cat_tall = fuzz.gaussian(height_in, 1.8, 0.5)
-    height_cat_average = fuzz.gaussian(height_in, 1.65, 0.5)
-    height_cat_short = fuzz.gaussian(height_in, 1.40, 0.5)
+    height_cat_tall = fuzz.gaussian(height_in, 1.8, 0.2)
+    height_cat_average = fuzz.gaussian(height_in, 1.65, 0.2)
+    height_cat_short = fuzz.gaussian(height_in, 1.40, 0.2)
     return dict(short = height_cat_short, average = height_cat_average, tall = height_cat_tall)
 
 def weight_category(weight_in):
-    weight_cat_skin = fuzz.triangular(weight_in, 30., 50., 65.)
-    weight_cat_fat = fuzz.triangular(weight_in, 50., 70., 90.)
-    return dict(skin = weight_cat_skin, fat = weight_cat_fat)
+    weight_cat_light = fuzz.gaussian(weight_in, 40, 10)
+    weight_cat_heavy = fuzz.gaussian(weight_in, 70, 10)
+    return dict(light = weight_cat_light, heavy = weight_cat_heavy)
 
-person1 = {
-    "name": "Hung",
-    "height": 1.69,
-    "weight": 60
-}
+#Fuzzy outputs
+def bmi_category(bmi_in):
+    bmi_under_weight = fuzz.triangular(bmi_in, 0., 8., 20.)
+    bmi_normal = fuzz.triangular(bmi_in, 18.0, 21.0, 27.0)
+    bmi_over_weight = fuzz.triangular(bmi_in, 25.0, 27.0, 33.0)
+    bmi_obese = fuzz.triangular(bmi_in, 30.0, 32.0, 38.0)
+    bmi_extremly_obese = fuzz.triangular(bmi_in, 35.0, 40.0, 50.0)
+    return dict(under_weight = bmi_under_weight, normal = bmi_normal, over_weight = bmi_over_weight, obese = bmi_obese, extremly_obese = bmi_extremly_obese)
 
+
+class Person:
+    name = str()
+    height = float()
+    weight = float()
+
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.weight = weight
+        self.height = height
+    
+    def getBMI(self):
+        return self.weight/(self.height * self.height)
+
+#RULES
 rules = [
-    #if height is short and weight is thin then size S
+    #if height is short and weight is light then 
 
-    #if height is short or weight is fat then size M
+    #if height is short or weight is heavy then 
 
-    #if height is short, weight is thin then size S
+    #if height is average, weight is light then 
 
-    #if height is short, weight is thin then size S
+    #if height is average, weight is heavy then 
 
-    #if height is short, weight is thin then size S
+    #if height is tall, weight is light then 
+
+    #if height is tall, weight is heavy then 
 ]
+
+#Define MF of fuzzy relation beetween height, short and result
+ 
+
+def print_result(person):
+    print("----------Hello " + person.name + "-------------")
+    print "For height: ", height_category(person.height)
+    print "For weight: ", weight_category(person.weight)
+    print "BMI: ", person.getBMI()
+    print "For BMI", bmi_category(person.getBMI())
+
 #main
 if __name__ == '__main__':
-    print("Hello " + person1["name"])
-    print "for height: ", height_category(person1["height"])
-    print "for weight: ", weight_category(person1["weight"])
+    person1 = Person("Hung", 1.4, 90)
+    print_result(person1)
     
